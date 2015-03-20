3b686d3fabcbc3e68c4cc608a1ff9193f681e713
Alex Scotton <alex@alexscotton.com>
2015-03-20 10:35:35 +0000

    Updates routes to have a configurable prefix

--- src/routes.php
+++ src/routes.php
@@ -7,7 +7,7 @@
 |
 */
 
-Route::group(array('prefix' => 'notifications'), function() {
+Route::group(array('prefix' => Config::get('notifications::config.route_prefix')), function() {
 
 	Route::get('/mark_as_read/{id}', 'Illizian\Notifications\Controllers\NotificationController@mark_as_read');
 
