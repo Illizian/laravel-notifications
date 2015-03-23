614234ff824fe8fb78cf4859fbac4a8843f12b39
Alex Scotton <alex@alexscotton.com>
2015-03-23 10:16:32 +0000

    Cleans up routes

--- src/routes.php
+++ src/routes.php
@@ -12,8 +12,8 @@ Route::group(array('prefix' => Config::get('notifications::config.route_prefix')
 	Route::group(array('prefix' => 'api'), function() {
 		
 		// JSON API
-		Route::get('all',		'Illizian\Notifications\Controllers\NotificationController@getNotificationJson');
-		Route::get('unread',	'Illizian\Notifications\Controllers\NotificationController@getUnreadNotificationJson');
+		Route::get('/all',		'Illizian\Notifications\Controllers\NotificationController@getNotificationJson');
+		Route::get('/unread',	'Illizian\Notifications\Controllers\NotificationController@getUnreadNotificationJson');
 
 	});
 
